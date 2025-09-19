8b2ed6d8b3254e8631968f90e72e5c34f428fb9c
awaisindhar <81686299+awaisindhar@users.noreply.github.com>
2025-09-19 13:46:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
+BOT_TOKEN = "7504366478:AAHboyc4Hxrf293CS2huyw7tdngLZkz2Hr0"
+ADMIN_ID  ="7050891491"
 # **BOT TOKEN & ADMIN ID**
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
